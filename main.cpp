e77acb6790180a46efd9351ae8ef532a53bbd0ed
Gavrilov Ivan <waine-86@yandex.ru>
2023-12-09 13:34:47 +0400

    поправил код

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 int main (int, char **) {
+    std::cout << "DZ - 1" << std::endl;
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;

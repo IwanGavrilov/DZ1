2e72a230fae5dd6ef2c3419db8372423b8291703
Gavrilov Ivan <waine-86@yandex.ru>
2023-12-09 14:12:09 +0400

    поправил код

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main (int, char **) {
     cout << "DZ - 1" << endl;
-    std::cout << "Version: " << version() << std::endl;
+    std::cout << "Version: " << version() << endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }

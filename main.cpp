4df5703763aba86640b7bb83f9d73cce276e2e47
Gavrilov Ivan <waine-86@yandex.ru>
2023-12-09 15:29:59 +0400

    code

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main (int, char **) {
     cout << "DZ - 1" << endl;
-    std::cout << "Version: " << version() << endl;
+    cout << "Version: " << version() << endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
